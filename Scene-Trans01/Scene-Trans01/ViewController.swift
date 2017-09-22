351fc0136c40400a63288b88b9915f05032fdac6
Andrew. C Lee <31760595+leebnc@users.noreply.github.com>
2017-09-22 15:15:34 +0900

    commit

---
+++ Scene-Trans01/Scene-Trans01/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Scene-Trans01
+//
+//  Created by Lee Choongwon on 2017. 9. 22..
+//  Copyright © 2017년 Choongwon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func moveNext(_ sender: Any) {
+    }
+}
+
