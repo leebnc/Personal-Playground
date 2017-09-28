4a694ff99db256bf32c64d1fbbcc05cb1bd74604
Andrew. C Lee <31760595+leebnc@users.noreply.github.com>
2017-09-28 15:45:14 +0900

    commit

---
+++ Scene-UnwindSegue/Scene-UnwindSegue/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Scene-UnwindSegue
+//
+//  Created by Lee Choongwon on 2017. 9. 28..
+//  Copyright © 2017년 Choongwon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func unwindToVC(_ segue: UIStoryboardSegue) {
+        
+    }
+}
+
