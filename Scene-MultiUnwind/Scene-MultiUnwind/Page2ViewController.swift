10b0618be352e8524964014a3e5bb2473ac53c06
Andrew. C Lee <31760595+leebnc@users.noreply.github.com>
2017-09-28 16:01:25 +0900

    commit

---
+++ Scene-MultiUnwind/Scene-MultiUnwind/Page2ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  Page2ViewController.swift
+//  Scene-MultiUnwind
+//
+//  Created by Lee Choongwon on 2017. 9. 28..
+//  Copyright © 2017년 Choongwon. All rights reserved.
+//
+
+import UIKit
+class Page2ViewController : UIViewController {
+    
+    
+    @IBAction func gotoPage2(_ sender: UIStoryboardSegue) {
+        
+    }
+    
+}
