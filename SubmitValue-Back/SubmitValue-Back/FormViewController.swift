401b694a96174511d4c54e7c62b14d30db73eec4
Andrew. C Lee <31760595+leebnc@users.noreply.github.com>
2017-09-29 17:37:23 +0900

    170929

--- SubmitValue-Back/SubmitValue-Back/FormViewController.swift
+++ SubmitValue-Back/SubmitValue-Back/FormViewController.swift
@@ -10,4 +10,41 @@ import UIKit
 
 class FormViewController : UIViewController {
     
+    @IBOutlet var email: UITextField!
+    
+    @IBOutlet var isUpdate: UISwitch!
+    
+    @IBOutlet var interval: UIStepper!
+    
+    @IBOutlet var isUpdateText: UILabel!
+    
+    @IBOutlet var intervalText: UILabel!
+    
+    @IBAction func onSwitch(_ sender: UISwitch) {
+        if (sender.isOn == true) {
+            self.isUpdateText.text = "갱신함"
+        } else {
+            self.isUpdateText.text = "갱신하지 않음"
+        }
+    }
+    @IBAction func onStepper(_ sender: UIStepper) {
+        let value = Int(sender.value)
+        self.intervalText.text = "\(value)분마다 갱신"
+    }
+    
+    @IBAction func onSubmit(_ sender: Any) {
+        
+        let preVC = self.presentingViewController
+        
+        guard let vc = preVC as? ViewController else {
+            return
+        }
+        
+        vc.paramEmail = self.email.text
+        vc.paramUpdate = self.isUpdate.isOn
+        vc.paramInterval = self.interval.value
+        
+        self.presentingViewController?.dismiss(animated: true)
+    }
+    
 }
