7546cde3100d9873ad01c6db20b1907b5e95d265
Andrew. C Lee <31760595+leebnc@users.noreply.github.com>
2017-10-07 17:22:23 +0900

    update

--- Msg-Notification/Msg-Notification/AppDelegate.swift
+++ Msg-Notification/Msg-Notification/AppDelegate.swift
@@ -17,15 +17,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        let setting = UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-        
-        application.registerUserNotificationSettings(setting)
-        
-        if let localNoti = launchOptions?[UIApplicationLaunchOptionsKey.localNotification] as? UILocalNotification {
-            // 알림으로 인해 앱이 실행된 경우.
-            print((localNoti.userInfo?["name"])!)
-        }
-        
+//        let setting = UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+//        
+//        application.registerUserNotificationSettings(setting)
+//        
+//        if let localNoti = launchOptions?[UIApplicationLaunchOptionsKey.localNotification] as? UILocalNotification {
+//            // 알림으로 인해 앱이 실행된 경우.
+//            print((localNoti.userInfo?["name"])!)
+//        }
+//        
         // Override point for customization after application launch.
         return true
     }
@@ -34,47 +34,47 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
         
-        if #available(iOS 10.0, *) {
-            let setting = application.currentUserNotificationSettings
-            guard setting?.types != .none else {
-                print("Can't Schedule")
-                return
-            }
-            
-            let nContent = UNMutableNotificationContent()
-            
-            nContent.badge = 1
-            nContent.body = "어서어서 들어오세요!!"
-            nContent.title = "로컬 알림 메세지"
-            nContent.subtitle = "서브타이틀"
-            nContent.sound = UNNotificationSound.default()
-            
-            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-            
-            let request = UNNotificationRequest(identifier: "Wake up", content: nContent, trigger: trigger)
-            
-            UNUserNotificationCenter.current().add(request)
-            
-        } else {
-            let setting = application.currentUserNotificationSettings
-            
-            guard setting?.types != .none else {
-                print("Can't Schedule")
-                return
-            }
-            
-            let noti = UILocalNotification()
-            
-            noti.fireDate = Date(timeIntervalSinceNow: 10)
-            noti.timeZone = TimeZone.autoupdatingCurrent
-            noti.alertBody = "얼른 다시 접속하세요!!!"
-            noti.alertAction = "학습하기"
-            noti.applicationIconBadgeNumber = 1
-            noti.soundName = UILocalNotificationDefaultSoundName
-            noti.userInfo = ["name":"홍길동"]
-            
-            application.scheduleLocalNotification(noti)
-        }
+//        if #available(iOS 10.0, *) {
+//            let setting = application.currentUserNotificationSettings
+//            guard setting?.types != .none else {
+//                print("Can't Schedule")
+//                return
+//            }
+//            
+//            let nContent = UNMutableNotificationContent()
+//            
+//            nContent.badge = 1
+//            nContent.body = "어서어서 들어오세요!!"
+//            nContent.title = "로컬 알림 메세지"
+//            nContent.subtitle = "서브타이틀"
+//            nContent.sound = UNNotificationSound.default()
+//            
+//            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+//            
+//            let request = UNNotificationRequest(identifier: "Wake up", content: nContent, trigger: trigger)
+//            
+//            UNUserNotificationCenter.current().add(request)
+//            
+//        } else {
+//            let setting = application.currentUserNotificationSettings
+//            
+//            guard setting?.types != .none else {
+//                print("Can't Schedule")
+//                return
+//            }
+//            
+//            let noti = UILocalNotification()
+//            
+//            noti.fireDate = Date(timeIntervalSinceNow: 10)
+//            noti.timeZone = TimeZone.autoupdatingCurrent
+//            noti.alertBody = "얼른 다시 접속하세요!!!"
+//            noti.alertAction = "학습하기"
+//            noti.applicationIconBadgeNumber = 1
+//            noti.soundName = UILocalNotificationDefaultSoundName
+//            noti.userInfo = ["name":"홍길동"]
+//            
+//            application.scheduleLocalNotification(noti)
+//        }
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
--- Msg-Notification/Msg-Notification/ViewController.swift
+++ Msg-Notification/Msg-Notification/ViewController.swift
@@ -7,19 +7,55 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet var msg: UITextField!
+    
+    @IBOutlet var datepicker: UIDatePicker!
+    
+    @IBAction func save(_ sender: Any) {
+        let setting = UIApplication.shared.currentUserNotificationSettings
+        
+        guard setting?.types != .none else {
+            let alert = UIAlertController(title: "알림 등록", message: "알림이 허용되어 있지 않습니다.", preferredStyle: .alert)
+            
+            let ok = UIAlertAction(title: "확인",  style: .default)
+            alert.addAction(ok)
+            
+            self.present(alert, animated: false)
+            return
+        }
+        
+        if #available(iOS 10, *) {
+            let nContent = UNMutableNotificationContent()
+            nContent.body = (self.msg.text)!
+            nContent.title = "미리알림"
+            nContent.sound = UNNotificationSound.default()
+            
+            let time = self.datepicker.date.timeIntervalSinceNow
+            
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: time, repeats: false)
+            
+            let request = UNNotificationRequest(identifier: "alarm", content: nContent, trigger: trigger)
+            
+            UNUserNotificationCenter.current().add(request) {
+                (_) in
+                
+                let date = self.datepicker.date.addingTimeInterval(9*60*60)
+                let message = "알림이 등록되었습니다. 등록된 알림은 \(date)에 발송됩니다."
+                let alert = UIAlertController(title: "알림등록", message: message, preferredStyle: .alert)
+                let ok = UIAlertAction(title:"확인", style: .default)
+                alert.addAction(ok)
+                
+                self.present(alert, animated: false)
+            }
+            
+            
+        } else {
+            
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
