68f33e3f8e50c94324a47a8d0d8d0c18b1718c07
Andrew. C Lee <31760595+leebnc@users.noreply.github.com>
2017-10-09 19:20:31 +0900

    update

--- MyMovieChart/MyMovieChart/ListViewController.swift
+++ MyMovieChart/MyMovieChart/ListViewController.swift
@@ -11,19 +11,20 @@ import UIKit
 class ListViewController : UITableViewController {
     
     var dataset = [
-        ("다크 나이트", "영웅물에 철학에 음악까지 더해져 예술이 되다", "2008-09-04", 8.95),
-        ("호우시절", "때를 알고 내리는 좋은 비", "2009-10-08", 7.31),
-        ("말할수 없는 비밀", "여기서 너까지 다섯 걸음", "2015-05-07", 9.19)
+        ("다크 나이트", "영웅물에 철학에 음악까지 더해져 예술이 되다", "2008-09-04", 8.95, "darknight.jpg"),
+        ("호우시절", "때를 알고 내리는 좋은 비", "2009-10-08", 7.31, "rain.jpg"),
+        ("말할수 없는 비밀", "여기서 너까지 다섯 걸음", "2015-05-07", 9.19, "secret.jpg")
     ]
     
     lazy var list : [MovieVO] = {
         var datalist = [MovieVO]()
-        for(title, desc, opendate, rating) in self.dataset {
+        for(title, desc, opendate, rating, thumbnail) in self.dataset {
             let mvo = MovieVO()
             mvo.title = title
             mvo.description = desc
             mvo.opendate = opendate
             mvo.rating = rating
+            mvo.thumbnail = thumbnail
             
             datalist.append(mvo)
         }
@@ -42,25 +43,14 @@ class ListViewController : UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let row = self.list[indexPath.row]
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell")!
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell")! as! MovieCell
         
-        let title = cell.viewWithTag(101) as? UILabel
+        cell.title?.text = row.title
+        cell.desc?.text = row.description
+        cell.opendate?.text = row.opendate
+        cell.rating?.text = "\(row.rating!)"
+        cell.thumbnail.image = UIImage(named: row.thumbnail!)
         
-        let desc = cell.viewWithTag(102) as? UILabel
-        
-        let opendate = cell.viewWithTag(103) as? UILabel
-        
-        let rating = cell.viewWithTag(104) as? UILabel
-        
-        title?.text = row.title
-        desc?.text = row.description
-        opendate?.text = row.opendate
-        rating?.text = "\(row.rating!)"
-        
-        
-//        cell.textLabel?.text = row.title
-//        
-//        cell.detailTextLabel?.text = row.description
         return cell
     }
     
--- MyMovieChart/MyMovieChart/MovieCell.swift
+++ MyMovieChart/MyMovieChart/MovieCell.swift
@@ -10,4 +10,13 @@ import UIKit
 
 class MovieCell : UITableViewCell {
     
+    @IBOutlet var title: UILabel!
+    
+    @IBOutlet var opendate: UILabel!
+    
+    @IBOutlet var desc: UILabel!
+    
+    @IBOutlet var rating: UILabel!
+    
+    @IBOutlet var thumbnail: UIImageView!
 }
