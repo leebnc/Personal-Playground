057ff9cbd7a5a3ee2f3853ac3a46304c014ddc22
Andrew. C Lee <31760595+leebnc@users.noreply.github.com>
2017-10-09 11:46:16 +0900

    update

---
+++ MyMovieChart/MyMovieChart/MovieVO.swift
@@ -0,0 +1,23 @@
+//
+//  MovieVO.swift
+//  MyMovieChart
+//
+//  Created by Lee Choongwon on 2017. 10. 9..
+//  Copyright © 2017년 Choongwon. All rights reserved.
+//
+
+import Foundation
+
+class MovieVO {
+    var thumbnail : String? // 영화 섬네일 이미지 주소
+    
+    var title : String? // 영화 제목
+    
+    var description : String? // 영화 설명
+    
+    var detail : String? // 상세정보
+    
+    var opendate : String? // 개봉일
+    
+    var rating : Double? // 평점
+}
