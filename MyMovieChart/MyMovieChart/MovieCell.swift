43babab70074f002d789312e26c53acbd9dd13ae
Andrew. C Lee <31760595+leebnc@users.noreply.github.com>
2017-10-09 13:26:45 +0900

    update

---
+++ MyMovieChart/MyMovieChart/MovieCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieCell.swift
+//  MyMovieChart
+//
+//  Created by Lee Choongwon on 2017. 10. 9..
+//  Copyright © 2017년 Choongwon. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell : UITableViewCell {
+    
+}
