ab70eaae4713a1dbe7edde93ffe138409fd301a8
Andrew. C Lee <31760595+leebnc@users.noreply.github.com>
2017-10-11 14:45:06 +0900

    update

--- MyMovieChart-SpinOff/MyMovieChart-SpinOff/ViewController.swift
+++ MyMovieChart-SpinOff/MyMovieChart-SpinOff/ViewController.swift
@@ -8,18 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.text = "\(indexPath.row)번째 데이터 입니다"
+        
+        return cell
     }
-
-
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        NSLog("\(indexPath.row) 번째 데이터가 클릭됨")
+    }
 }
 
