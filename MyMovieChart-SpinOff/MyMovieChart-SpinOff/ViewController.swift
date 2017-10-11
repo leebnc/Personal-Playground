a9dea1c0c1577d07d28e38d8aeb3797a1dbb9a43
Andrew. C Lee <31760595+leebnc@users.noreply.github.com>
2017-10-11 14:31:41 +0900

    update

---
+++ MyMovieChart-SpinOff/MyMovieChart-SpinOff/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyMovieChart-SpinOff
+//
+//  Created by Lee Choongwon on 2017. 10. 11..
+//  Copyright © 2017년 Choongwon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
